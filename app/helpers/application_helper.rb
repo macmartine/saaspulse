b462becf1238b3c9e98f1b15bffb7453f1d3d9c6
Mac Martine <mac@macmartine.com>
2023-09-26 08:23:21 +0200

    Email styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,11 @@ module ApplicationHelper
     :light
   end
 
-  def currency_formatter(value)
-    # op = value.to_i >= 0 ? '+' : ''
+  def currency_formatter(value, is_potential = false)
+
+    if is_potential
+      return "#{up_yellow}#{number_to_currency(value.to_i, precision: 0).gsub('-', '')}"
+    end
     op = value.to_i >= 0 ? up : down
     puts "OP: #{op}"
     return "#{op}#{number_to_currency(value.to_i, precision: 0).gsub('-', '')}"
@@ -19,21 +22,18 @@ module ApplicationHelper
   end
 
   def up
-    "<img src='https://app.saaspulse.io/assets/arrow-up-right.png' />"
+    "<img src='https://app.saaspulse.io/images/arrow-up-right.png' style='vertical-align: middle; margin-right: 2px'/>"
   end
 
   def down
-    "<img src='https://app.saaspulse.io/assets/arrow-down-right.png' />"
+    "<img src='https://app.saaspulse.io/images/arrow-down-right.png' style='vertical-align: middle; margin-right: 2px'/>"
   end
 
-  # def email_icon
-  #   '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="16px">
-  #     <path stroke-linecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
-  #   </svg>'
-  # end
+  def up_yellow
+    "<img src='https://app.saaspulse.io/images/arrow-up-right-yellow.png' style='vertical-align: middle; margin-right: 2px'/>"
+  end
 
   def stripe_icon
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="#6772e5"><path d="M111.328 15.602c0-4.97-2.415-8.9-7.013-8.9s-7.423 3.924-7.423 8.863c0 5.85 3.32 8.8 8.036 8.8 2.318 0 4.06-.528 5.377-1.26V19.22a10.246 10.246 0 0 1-4.764 1.075c-1.9 0-3.556-.67-3.774-2.943h9.497a39.64 39.64 0 0 0 .063-1.748zm-9.606-1.835c0-2.186 1.35-3.1 2.56-3.1s2.454.906 2.454 3.1zM89.4 6.712a5.434 5.434 0 0 0-3.801 1.509l-.254-1.208h-4.27v22.64l4.85-1.032v-5.488a5.434 5.434 0 0 0 3.444 1.265c3.472 0 6.64-2.792 6.64-8.957.003-5.66-3.206-8.73-6.614-8.73zM88.23 20.1a2.898 2.898 0 0 1-2.288-.906l-.03-7.2a2.928 2.928 0 0 1 2.315-.96c1.775 0 2.998 2 2.998 4.528.003 2.593-1.198 4.546-2.995 4.546zM79.25.57l-4.87 1.035v3.95l4.87-1.032z" fill-rule="evenodd"/><path d="M74.38 7.035h4.87V24.04h-4.87z"/><path d="M69.164 8.47l-.302-1.434h-4.196V24.04h4.848V12.5c1.147-1.5 3.082-1.208 3.698-1.017V7.038c-.646-.232-2.913-.658-4.048 1.43zm-9.73-5.646L54.698 3.83l-.02 15.562c0 2.87 2.158 4.993 5.038 4.993 1.585 0 2.756-.302 3.405-.643v-3.95c-.622.248-3.683 1.138-3.683-1.72v-6.9h3.683V7.035h-3.683zM46.3 11.97c0-.758.63-1.05 1.648-1.05a10.868 10.868 0 0 1 4.83 1.25V7.6a12.815 12.815 0 0 0-4.83-.888c-3.924 0-6.557 2.056-6.557 5.488 0 5.37 7.375 4.498 7.375 6.813 0 .906-.78 1.186-1.863 1.186-1.606 0-3.68-.664-5.307-1.55v4.63a13.461 13.461 0 0 0 5.307 1.117c4.033 0 6.813-1.992 6.813-5.485 0-5.796-7.417-4.76-7.417-6.943zM13.88 9.515c0-1.37 1.14-1.9 2.982-1.9A19.661 19.661 0 0 1 25.6 9.876v-8.27A23.184 23.184 0 0 0 16.862.001C9.762.001 5 3.72 5 9.93c0 9.716 13.342 8.138 13.342 12.326 0 1.638-1.4 2.146-3.37 2.146-2.905 0-6.657-1.202-9.6-2.802v8.378A24.353 24.353 0 0 0 14.973 32C22.27 32 27.3 28.395 27.3 22.077c0-10.486-13.42-8.613-13.42-12.56z" fill-rule="evenodd"/></svg>'
   end
 
   def change_class(value, is_potential = false)
@@ -42,7 +42,7 @@ module ApplicationHelper
     if value.is_a?(Integer)
       return value >= 0 ? 'green' : 'red'
     else
-      return value.include?('green') ? 'green' : 'red'
+      return value.include?('down') ? 'red' : 'green'
     end
   end
 
